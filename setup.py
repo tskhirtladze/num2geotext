0b07d663efb1d543baf3e4a1ddf64165b6b5167d
skhir <skhirtladze.tornike@gmail.com>
2024-10-11 16:36:35 +0400

    Updated version number in setup.py to 0.0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from pathlib import Path
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 DESCRIPTION = '''
 A Python package for converting numbers and floats (up to 15 digits) into Georgian text, 
 and for converting floats (up to 15 digits) into Georgian currency text representations.
